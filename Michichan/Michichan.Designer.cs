579b945e82567ccd98ef5c5597922f23d78b319a
Miguel <loranvallsmiguel@gmail.com>
2024-12-15 13:13:10 +0100

    Initial commit

--- Michichan/Michichan.Designer.cs
+++ Michichan/Michichan.Designer.cs
@@ -24,16 +24,84 @@ partial class Michichan
     #region Windows Form Designer generated code
 
     /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        label1 = new System.Windows.Forms.Label();
+        label2 = new System.Windows.Forms.Label();
+        button1 = new System.Windows.Forms.Button();
+        button2 = new System.Windows.Forms.Button();
+        SuspendLayout();
+        // 
+        // label1
+        // 
+        label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        label1.Location = new System.Drawing.Point(88, 50);
+        label1.Name = "label1";
+        label1.Size = new System.Drawing.Size(464, 98);
+        label1.TabIndex = 0;
+        label1.Text = "countdown";
+        // 
+        // label2
+        // 
+        label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        label2.Location = new System.Drawing.Point(336, 45);
+        label2.Name = "label2";
+        label2.Size = new System.Drawing.Size(166, 66);
+        label2.TabIndex = 1;
+        label2.Text = "label2";
+        // 
+        // button1
+        // 
+        button1.BackColor = System.Drawing.Color.Yellow;
+        button1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        button1.Location = new System.Drawing.Point(173, 151);
+        button1.Name = "button1";
+        button1.Size = new System.Drawing.Size(235, 64);
+        button1.TabIndex = 2;
+        button1.Text = "HOLD!!!!!!";
+        button1.UseVisualStyleBackColor = false;
+        button1.Click += button1_Click;
+        // 
+        // button2
+        // 
+        button2.BackColor = System.Drawing.Color.OrangeRed;
+        button2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        button2.Location = new System.Drawing.Point(464, 151);
+        button2.Name = "button2";
+        button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+        button2.Size = new System.Drawing.Size(235, 64);
+        button2.TabIndex = 3;
+        button2.Text = "ABORT";
+        button2.UseVisualStyleBackColor = false;
+        button2.Click += button2_Click;
+        // 
+        // Michichan
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(1902, 1033);
+        Controls.Add(button2);
+        Controls.Add(button1);
+        Controls.Add(label2);
+        Controls.Add(label1);
+        Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        MaximizeBox = false;
+        StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        Text = "Michichan";
+        WindowState = System.Windows.Forms.FormWindowState.Maximized;
+        ResumeLayout(false);
     }
+
+    private System.Windows.Forms.Button button2;
+
+    private System.Windows.Forms.Button button1;
+
+    private System.Windows.Forms.Label label2;
+
+    private System.Windows.Forms.Label label1;
 
     #endregion
 }
--- Michichan/Michichan.cs
+++ Michichan/Michichan.cs
@@ -1,9 +1,82 @@
+using Timer = System.Windows.Forms.Timer;
+
 namespace Michichan;
 
-public partial class Form1 : Form
+public partial class Michichan : Form
 {
-    public Form1()
+    private TimeSpan _countdownSpan;
+    private TimeSpan _countdown;
+    
+    private Timer _timer = new();
+    private DateTime _startTime;
+
+    private bool _holding;
+    private bool _running;
+    
+    public Michichan()
     {
         InitializeComponent();
+        InitializeCountdown(TimeSpan.FromSeconds(-20));
+    }
+
+    private void timer_Tick(object? sender, EventArgs e) => UpdateCountdown();
+
+    private void UpdateCountdown()
+    {
+        if (!_running)
+        {
+            label1.Text = "T--:--";
+            label2.Text = "000";
+            return;
+        }
+        
+        var elapsed = DateTime.Now - _startTime;
+        _countdown = _countdownSpan.Add(elapsed);
+            
+        TimeSpan displayTime = TimeSpan.FromTicks(Math.Abs(_countdown.Ticks));
+            
+        if (_countdown != displayTime)
+            displayTime = displayTime.Add(TimeSpan.FromSeconds(1));
+            
+        string prefix = _countdown == displayTime ? "T+" : "T-";
+            
+        label1.Text = $"{prefix}{displayTime.Minutes:D2}:{displayTime.Seconds:D2}";
+        label2.Text = $"{displayTime.Milliseconds:D3}";
+    }
+
+    private void InitializeCountdown(TimeSpan span)
+    {
+        _countdownSpan = span;
+        _startTime = DateTime.Now;
+        
+        _timer.Interval = 10;   
+        _timer.Start();
+
+        _timer.Tick += timer_Tick;
+        _holding = false;
+        _running = true;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        if (!_holding)
+        {
+            _holding = true;
+            _timer.Stop();
+        }
+        else
+        {
+            _holding = false;
+            _countdownSpan = _countdown;
+            _startTime = DateTime.Now;
+            _timer.Start();
+        }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        _timer.Stop();
+        _running = false;
+        UpdateCountdown();
     }
 }
